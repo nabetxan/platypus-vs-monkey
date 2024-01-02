46d61c8875719c8d8ac7be9b2be6a0b8e6304145
nabetxan <119922401+nabetxan@users.noreply.github.com>
2024-01-02 11:34:14 +0900

    feat(wip): migration to vite and typescript

--- src/Components/Board/Board.tsx
+++ src/Components/Board/Board.tsx
@@ -1,6 +1,25 @@
 import { GameStatus, checkWin, isCellEmpty } from "../Utils/GameStrategy";
 import Cell, { Gameboard } from "./Cell";
 
+// check if the cell is a winnerCell
+const isWinnerCell = function (
+  r: number,
+  c: number,
+  gameboard: Gameboard,
+  gameStatus: GameStatus
+) {
+  if (!gameStatus.winner) return false;
+  const winnerObj = checkWin(gameboard);
+  if (!winnerObj) return false;
+  const winnerPos = winnerObj.winnerPos;
+  if (
+    (winnerPos[0][0] === r && winnerPos[0][1] === c) ||
+    (winnerPos[1][0] === r && winnerPos[1][1] === c) ||
+    (winnerPos[2][0] === r && winnerPos[2][1] === c)
+  )
+    return true;
+};
+
 const Board: React.FC<{
   gameboard: Gameboard;
   gameStatus: GameStatus;
@@ -9,20 +28,9 @@ const Board: React.FC<{
   const currentPlayer = gameStatus.isP1CurrentPlayer
     ? gameStatus.P1
     : gameStatus.P2;
-  const PnP = gameStatus.selectedPnP;
-
-  // check if the cell is a winnerCell
-  const isWinnerCell = function (r: number, c: number) {
-    if (!gameStatus.winner) return false;
-    const winnerObj = checkWin(gameboard);
-    if (!winnerObj) return false;
-    const winnerPos = winnerObj.winnerPos;
-    if (
-      (winnerPos[0][0] === r && winnerPos[0][1] === c) ||
-      (winnerPos[1][0] === r && winnerPos[1][1] === c) ||
-      (winnerPos[2][0] === r && winnerPos[2][1] === c)
-    )
-      return true;
+  const PnP = gameStatus.selectedPnP ?? {
+    index: undefined,
+    isSelectedFromBoard: false
   };
 
   const handleCellClick = function (cell: Cell, r: number, c: number) {
@@ -30,131 +38,110 @@ const Board: React.FC<{
     if (gameStatus.winner) return;
 
     // when nothing is selected for selectedPieceAndPlayer
-    // you clicked an empty cell? --> noop
-    // you clicked an cell with opponent char on top? --> noop
-    if (!PnP?.index) {
-      if (isCellEmpty(cell) || cell.pieces[0]?.player !== currentPlayer) {
+    // clicked an empty cell? --> noop
+    // clicked an cell with opponent char on top? --> noop
+    // clicked an cell with your character on top? --> select
+    if (PnP.index === undefined) {
+      if (isCellEmpty(cell) || cell.pieces[0].player !== currentPlayer) {
         return;
-      } else if (cell.pieces[0]?.player === currentPlayer) {
-        // you clicked an cell with your character on top? --> select
-        const swapPieceIndex = cell.pieces[0].index;
-        const swapPieceSize = cell.pieces[0].size;
+      } else {
         const pieceInfo = cell.pieces[0];
         const newGameStatus = {
           ...gameStatus,
           selectedPnP: {
-            index: swapPieceIndex,
+            index: pieceInfo.index,
             currentPlayer: currentPlayer,
-            pieceSize: swapPieceSize,
+            pieceSize: pieceInfo.size,
             isSelectedFromBoard: true,
             positionOnBoard: [r, c],
             pieceInfo: pieceInfo
           }
         };
-        onChange({ ...gameboard }, newGameStatus);
+        onChange(gameboard, newGameStatus);
         return;
       }
     }
 
-    // when a piece is already selected from hand pieces
-    if (!PnP?.isSelectedFromBoard) {
-      // when the cell is not empty and and the piece is equal or bigger --> noop
-      if (!isCellEmpty(cell)) {
-        if (PnP?.pieceSize === "small") return;
-        if (PnP?.pieceSize === "medium" && cell.pieces[0].size !== "small") {
-          return;
-        }
-        if (
-          PnP?.pieceSize === "large" &&
+    // clicked non-empty cell and and the existing piece is equal or bigger? --> noop
+    if (
+      !isCellEmpty(cell) &&
+      (PnP.pieceSize === "small" ||
+        (PnP.pieceSize === "medium" && cell.pieces[0].size !== "small") ||
+        (PnP.pieceSize === "large" &&
           cell.pieces[0].size !== "small" &&
-          cell.pieces[0].size !== "medium"
-        ) {
-          return;
-        }
-      }
-      // in other cases (if it's smaller or the cell is empty), play the piece
+          cell.pieces[0].size !== "medium"))
+    )
+      return;
 
-      const updatedGameboard = [...gameboard];
-
-      const pieceIndex = currentPlayer.piece.findIndex(
-        (piece) => piece.index === PnP?.index
-      );
-      updatedGameboard[r][c].pieces.unshift(currentPlayer.piece[pieceIndex]);
-      const updatePlayerPiece = [...currentPlayer.piece];
-      updatePlayerPiece.splice(pieceIndex, 1);
-      currentPlayer.piece = updatePlayerPiece;
-      // TODO: あってる？？
-      //元々setPlayer2Piece(updatePlayer2Piece);のように管理していた
-      const newGameStatus = {
-        ...gameStatus,
-        isP1CurrentPlayer: gameStatus.isP1CurrentPlayer ? false : true
-      };
-      onChange(updatedGameboard, newGameStatus);
-
-      const winnerPos = checkWin(updatedGameboard);
-
-      if (winnerPos) {
-        const newGameStatus = {
-          ...gameStatus,
-          winner: winnerPos.winner
-        };
-        onChange({ ...gameboard }, newGameStatus);
-
-        // if (winnerPos.winner === P1) {
-        //   const newScore = scoreKeep[0] + 1;
-        //   setScoreKeep([newScore, scoreKeep[1]]);
-        //   addConfetti(document.getElementById("score-board"));
-        // } else {
-        //   const newScore = scoreKeep[1] + 1;
-        //   setScoreKeep([scoreKeep[0], newScore]);
-        //   addConfetti(document.getElementById("score-board"));
-        // }
-      }
-    } else if (PnP.isSelectedFromBoard) {
-      // when a piece is already selected from the board
-      // when the cell is not empty and and the piece is equal or bigger --> noop
-
-      if (
-        !isCellEmpty(cell) &&
-        (PnP.pieceSize === "small" ||
-          (PnP.pieceSize === "medium" && cell.pieces[0].size !== "small") ||
-          (PnP.pieceSize === "large" &&
-            cell.pieces[0].size !== "small" &&
-            cell.pieces[0].size !== "medium"))
-      )
-        return;
-
-      // in other cases (if it's smaller or the cell is empty),
-      // first remove the piece from the original cell
-
-      const updatedGameboard = [...gameboard];
-
+    // in other cases (if it's smaller or the cell is empty)? --> play the piece
+    const updatedGameboard = [...gameboard];
+    // piece selected from the board? --> first remove the piece from the original cell
+    if (PnP.isSelectedFromBoard) {
       PnP.positionOnBoard &&
         updatedGameboard[PnP.positionOnBoard[0]][
           PnP.positionOnBoard[1]
         ].pieces.shift();
-
-      //then, play the piece
-
       PnP.pieceInfo && updatedGameboard[r][c].pieces.unshift(PnP.pieceInfo);
+    } else {
+      // piece selected from hand pieces?
+      const targetIndex = currentPlayer.piece.findIndex(
+        (piece) => piece.index === PnP.index
+      );
+      updatedGameboard[r][c].pieces.unshift(currentPlayer.piece[targetIndex]);
+      const updatePlayerPiece = [...currentPlayer.piece];
+      updatePlayerPiece.splice(targetIndex, 1);
+      currentPlayer.piece = updatePlayerPiece;
+    }
+    //then, play the piece
+    const updatedGameStatus = {
+      ...gameStatus,
+      selectedPnP: {
+        ...gameStatus.selectedPnP,
+        index: undefined,
+        currentPlayer: undefined,
+        pieceSize: undefined,
+        isSelectedFromBoard: undefined,
+        positionOnBoard: undefined
+      },
+      isP1CurrentPlayer: gameStatus.isP1CurrentPlayer ? false : true
+    };
+    onChange(updatedGameboard, updatedGameStatus);
 
+    const winnerPos = checkWin(updatedGameboard);
+
+    if (winnerPos) {
       const newGameStatus = {
         ...gameStatus,
-        isP1CurrentPlayer: gameStatus.isP1CurrentPlayer ? false : true,
-        selectedPnP: {}
+        winner: winnerPos.winner
       };
       onChange(updatedGameboard, newGameStatus);
+    }
+    // if (winnerPos.winner === P1) {
+    //   const newScore = scoreKeep[0] + 1;
+    //   setScoreKeep([newScore, scoreKeep[1]]);
+    //   addConfetti(document.getElementById("score-board"));
+    // } else {
+    //   const newScore = scoreKeep[1] + 1;
+    //   setScoreKeep([scoreKeep[0], newScore]);
+    //   addConfetti(document.getElementById("score-board"));
+    // }
+  };
 
-      const winnerPos = checkWin(updatedGameboard);
+  const handleClassName = function (cell: Cell, r: number, c: number) {
+    let classname = "cell";
 
-      if (winnerPos) {
-        const newGameStatus = {
-          ...gameStatus,
-          winner: winnerPos.winner
-        };
-        onChange(updatedGameboard, newGameStatus);
+    if (PnP?.positionOnBoard) {
+      if (PnP.positionOnBoard[0] === r && PnP.positionOnBoard[1] === c) {
+        classname = classname + " selected-cell";
       }
     }
+    classname = isWinnerCell(r, c, gameboard, gameStatus)
+      ? classname + " win"
+      : classname;
+    if (cell.pieces[0]) {
+      classname = classname + " " + cell.pieces[0].size;
+    }
+    return classname;
   };
 
   return (
@@ -163,25 +150,9 @@ const Board: React.FC<{
         return (
           <div className="row" key={r}>
             {row.map((cell, c) => {
-              let classname = "cell";
-              // if a piece is selected from the board, highlight it
-              if (PnP?.positionOnBoard) {
-                if (
-                  PnP.positionOnBoard[0] === r &&
-                  PnP.positionOnBoard[1] === c
-                ) {
-                  classname = classname + " selected-cell";
-                }
-              }
-
-              classname = isWinnerCell(r, c) ? classname + " win" : classname;
-
-              if (cell.pieces[0]) {
-                classname = classname + " " + cell.pieces[0].size;
-              }
               return (
                 <div
-                  className={classname}
+                  className={handleClassName(cell, r, c)}
                   onClick={() => handleCellClick(cell, r, c)}
                   key={`${r}-${c}`}
                 >
