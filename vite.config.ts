98b228fcb55d000eb225f5e7fc9c94d7fd92c5eb
nabetxan <119922401+nabetxan@users.noreply.github.com>
2024-01-15 16:29:52 +0900

    feat: add doc folder for github page

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./"
+  base: "./",
+  build: {
+    outDir: "./docs"
+  }
 });
